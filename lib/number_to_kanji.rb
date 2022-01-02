06497a880a7c7268d975de10b59c548834cf90d3
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-01-02 15:39:58 +0900

    数字を漢字に変換する仕組みを作った。

--- lib/number_to_kanji.rb
+++ lib/number_to_kanji.rb
@@ -1,7 +1,34 @@
 # frozen_string_literal: true
 
-require_relative "number_to_kanji/version"
+require_relative 'number_to_kanji/kanji_map'
 require_relative "number_to_kanji/small_number_to_kanji"
+require_relative "number_to_kanji/version"
 
 module NumberToKanji
+  def call(number)
+    raise TypeError unless number.is_a?(Integer)
+
+    return KANJI_MAP[0] if number.zero?
+
+    # 四桁ごとに区切りつつ、位の小さい順に並んだ配列を作る。
+    # 例: 123456789 => [6789, 2345, 1]
+    # 例: 100006789 => [6789, 0, 1]
+    separated_numbers = number.to_s.chars.reverse.each_slice(4).map(&:reverse).map(&:join).map(&:to_i)
+
+    separated_numbers.map.with_index do |separated_number, index|
+      _kanji_number_with_unit(separated_number, index)
+    end.reverse.join
+  end
+
+  def _kanji_number_with_unit(number, index)
+    return nil if number.zero?
+
+    kanji = SmallNumberToKanji.call(number)
+    unit = KANJI_UNITS[index]
+
+    "#{kanji}#{unit}"
+  end
+
+  module_function :call, :_kanji_number_with_unit
+  private_class_method :_kanji_number_with_unit
 end
--- lib/number_to_kanji/kanji_map.rb
+++ lib/number_to_kanji/kanji_map.rb
@@ -1,4 +1,5 @@
 module NumberToKanji
   KANJI_MAP = { 0 => "〇", 1 => "一", 2 => "二", 3 => "三", 4 => "四",
                 5 => "五", 6 => "六", 7 => "七", 8 => "八", 9 => "九" }.freeze
+  KANJI_UNITS = ['', '万', '億', '兆', '京', '垓'].freeze
 end
